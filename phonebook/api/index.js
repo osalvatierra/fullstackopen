b987824298bc023d9815b13f8d6052ef35867cb9
osalvatierra <oscar@owebdesigns.com>
2025-06-25 10:50:30 -0400

    adding middleware for handling errors globally

--- phonebook/api/index.js
+++ phonebook/api/index.js
@@ -131,6 +131,20 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.name, error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
