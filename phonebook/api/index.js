215c209d91d9d609e8880820bbdc7b82411633bc
osalvatierra <oscar@owebdesigns.com>
2025-07-07 15:37:48 -0400

    re-adding back cors, removed by mistake v4

--- phonebook/api/index.js
+++ phonebook/api/index.js
@@ -1,8 +1,6 @@
-import express from 'express'
+import app from './app.js'
 import config from './util/config.js'
 import logger from './util/logger.js'
-
-const app = express()
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
